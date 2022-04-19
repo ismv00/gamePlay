76c89a88801f06bc140d967d49e07e5ecc6189dc
Igor Menezes <ismv00@icloud.com>
2022-04-18 22:55:47 -0400

    create-duelo

--- src/components/Appointments/index.tsx
+++ src/components/Appointments/index.tsx
@@ -1,78 +1,79 @@
-import { View, Text } from "react-native";
-import { RectButton, RectButtonProps  } from "react-native-gesture-handler";
-import { categories } from "../../Utils/categories";
-import { GuildIcon } from "../GuildIcon";
+import React from 'react';
+import { LinearGradient } from 'expo-linear-gradient';
+import { RectButton, RectButtonProps } from 'react-native-gesture-handler';
+import { View, Text } from 'react-native';
+
+import { theme } from '../../global/styles/theme';
+import { styles } from './styles';
 import PlayerSvg from '../../assets/player.svg';
 import CalendarSvg from '../../assets/calendar.svg';
 
-import { styles } from "./styles";
-import { theme } from "../../global/styles/theme";
-import { GuildProps } from "../Guild";
-import { LinearGradient } from "expo-linear-gradient";
+import { GuildProps } from '../Guild';
+import { GuildIcon } from '../GuildIcon';
+import { categories } from '../../Utils/categories';
 
-export type AppointmentsProps = {
-    id: string;
-    guild: GuildProps;
-    category: string;
-    date: string;
-    description: string
+
+export type AppointmentProps = {
+  id: string;
+  guild: GuildProps;
+  category: string;
+  date: string;
+  description: string;
 }
 
 type Props = RectButtonProps & {
-    data: AppointmentsProps;
-}
-export function Appointments( {data, ...rest} : Props ) {
+  data: AppointmentProps;
+} 
 
-    const [category] = categories.filter(item => item.id === data.category);
-    const { owner } = data.guild; 
-    const { primary, on, secondary50, secondary70 } = theme.colors;
+export function Appointment({ data, ...rest }: Props) {
+  const [category] = categories.filter(item => item.id === data.category);
+  const { owner } = data.guild;
+  const { primary, on, secondary50, secondary70 } = theme.colors;
 
-    return (
-        <RectButton 
-            {...rest}
+  return (
+    <RectButton {...rest}>
+      <View style={styles.container}>
+        <LinearGradient 
+          style={styles.guildIconContainer}
+          colors={[secondary50, secondary70]}
         >
-            <View style={styles.container}>
-                <LinearGradient
-                    style={styles.guildIconContainer}
-                    colors={[secondary50, secondary70 ]}
-                >
-                    <GuildIcon />
-                </LinearGradient>
+          <GuildIcon guildId={data.guild.id} iconId={data.guild.icon} />
+        </LinearGradient>
 
-                <View style={styles.content}>
-                    <View style={styles.header}>
-                        <Text style={styles.title}>
-                            { data.guild.name }
-                        </Text>
+        <View style={styles.content}>
+          <View style={styles.header}>
+            <Text style={styles.title}>
+              { data.guild.name }
+            </Text>
 
-                        <Text style={styles.category}>
-                            { category.title }
-                        </Text>
-                    </View>
+            <Text style={styles.category}>
+              { category.title }
+            </Text>
+          </View>
 
-                    <View style={styles.footer}>
-                        <View style={styles.dateInfo}>
-                            <CalendarSvg />
+          <View style={styles.footer}>
+            <View style={styles.dateInfo}>
+              <CalendarSvg />
 
-                            <Text style={styles.date}>
-                                { data.date }
-                            </Text>
-                        </View>
-
-                        <View style={styles.playersInfo}>
-                            <PlayerSvg fill={ owner? primary : on} />
-
-                            <Text style={[
-                                styles.player, { color: owner ? primary : on }
-                            ]}
-                            >
-                                { owner ? 'Anfitrião' : 'Visitante' }
-                            </Text>
-                        </View>    
-                    </View> 
-                </View>
+              <Text style={styles.date}>
+                { data.date }                
+              </Text>
             </View>
 
-        </RectButton>
-    )
+            <View style={styles.playersInfo}>
+              <PlayerSvg fill={ owner ? primary : on}/>
+
+              <Text style={[
+                styles.player, 
+                { color: owner ? primary : on }
+              ]}>
+                { owner ? 'Anfitrião' : 'Visitante' }
+              </Text>
+            </View>          
+          </View>
+        </View>
+      </View>
+    </RectButton>
+  )
+
 }
